889c09d12caf2dc3042f64d0030a0dcc813d7644
Scotch <mtz_2830@yahoo.co.jp>
2020-02-14 04:32:11 +0000

    Add variable

--- app.rb
+++ app.rb
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <title>おみくじ</title>
-    </head>
-    <body>
-        <h1>おみくじ</h1>
-        <p>おみくじの結果：無し</p>
-        <form action="/" method="POST">
-            <button type="submit" name="act" value="draw">おみくじをひく！</button>
-        </form>
-    </body>
-</html>
+require 'sinatra'
+
+get '/' do
+  erb :index
+end
